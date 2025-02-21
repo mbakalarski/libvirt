e89103442a196954dc46f1f78b78c6420ad996c3
mbk <mbakalarski at github>
2025-02-21 12:49:05 +0100

    1st commit

---
+++ LIBVIRT_install.sh
@@ -0,0 +1,4 @@
+sudo apt install --no-install-recommends qemu-system libvirt-clients libvirt-daemon-system
+sudo apt install --no-install-recommends virtinst
+sudo apt install --no-install-recommends qemu-utils
+
