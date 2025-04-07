6f7f74a031a115a9d9539bf6d8ae25473e3eb15d
mbk <mbakalarski at github>
2025-04-07 15:32:14 +0200

    centos VM better script

---
+++ centos/VM_centos_install.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -e
+
+
+if [[ -f CentOS-Stream-GenericCloud-x86_64-10-latest.x86_64.qcow2 ]]; then :; else
+wget https://cloud.centos.org/centos/10-stream/x86_64/images/CentOS-Stream-GenericCloud-x86_64-10-latest.x86_64.qcow2
+fi
+
+if [[ -f CentOS-Stream-GenericCloud-x86_64-10-latest.x86_64.qcow2.SHA256SUM ]]; then :; else
+wget https://cloud.centos.org/centos/10-stream/x86_64/images/CentOS-Stream-GenericCloud-x86_64-10-latest.x86_64.qcow2.SHA256SUM
+fi
+
+sha256sum -c CentOS-Stream-GenericCloud-x86_64-10-latest.x86_64.qcow2.SHA256SUM
+
+backing_file="CentOS-Stream-GenericCloud-x86_64-10-latest.x86_64.qcow2"
+qemu-img create -f qcow2 -b ${backing_file} -F qcow2 centos1-01.qcow2 20G
+
+qemu-img create -f qcow2 centos1-02.qcow2 20G
+qemu-img create -f qcow2 centos1-03.qcow2 20G
+
+
+###
+PASSWORD=lab12345
+
+
+cat << EOT > centos1-user-data
+#cloud-config
+
+chpasswd:
+  expire: false
+  users:
+  - {name: root, password: ${PASSWORD}, type: text}
+
+runcmd:
+  - hostnamectl set-hostname centos1
+  - #
+  - rm -f /etc/ssh/sshd_config.d/50-cloud-init.conf || true
+  - echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+  - systemctl restart sshd
+  - #
+EOT
+
+###
+
+
+virt-install --virt-type kvm --name centos1 \
+--osinfo centos-stream9 \
+--import \
+--memory 2048 --vcpu 2 \
+--disk ./centos1-01.qcow2 \
+--disk ./centos1-02.qcow2 \
+--disk ./centos1-03.qcow2 \
+--graphics none \
+--console pty,target_type=serial \
+--cpu host \
+--hvm \
+--cloud-init user-data=./centos1-user-data
+
